248426dd8c3ec55d4e5cd6dd54aab2792677c36c
Max <maksym2007petryk@gmail.com>
2026-02-13 22:12:34 +0200

    вул

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,8 @@ const CopyWebpackPlugin = require('copy-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
 const CssMinimizerWebpackPlugin = require('css-minimizer-webpack-plugin');
 
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
 const optimization = () => ({
     splitChunks: {
         chunks: 'all',
@@ -21,32 +23,45 @@ module.exports = {
     entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, './dist'),
-        filename: './[name].bundle.js',
+        filename: '[name].[contenthash].js',
     },
     module: {
         rules: [
-            {test: /\.css$/i, use:['style-loader', 'css-loader']},
+            {test: /\.css$/i, use:[MiniCssExtractPlugin.loader, 'css-loader']},
             {test: /\.ts$/i, loader: 'ts-loader'},
             {test:  /\.(jpg|png|svg|jpeg|gif)$/,
                 type: 'asset/resource',
                 generator: {
-                    filename: 'assets/images/[name].[hash][ext]', // Шаблон імені файлу для зображень
+                    filename: 'assets/images/[name].[contenthash][ext]',
                 }},
-            {test: /\\.(woff|woff2|ttf|eot)$/i,
+            {test: /\.(woff|woff2|ttf|eot)$/i,
             type: 'asset/resource',
             generator: {
-            filename: 'assets/fonts/[name].[hash][ext]', }
+            filename: 'assets/fonts/[name].[contenthash][ext]', }
              }
         ]
     },
     plugins: [
         new HtmlWebpackPlugin({template: './src/index.html'}),
         new CleanWebpackPlugin(),
-        new CopyWebpackPlugin({
-            patterns: [
-                { from: 'src/images', to: 'images' }
-            ],
+        new MiniCssExtractPlugin({
+            filename: '[name].[contenthash].css'
         }),
+
+        /* new CopyWebpackPlugin({
+             patterns: [
+                 { from: 'src/images', to: 'images' }
+             ],
+         }),*/
     ],
     optimization: optimization(),
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'dist'),
+        },
+        compress: true,
+        port: 9000,
+        open: true,
+        hot: true,
+    }
 }
